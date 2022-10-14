b440e5167afe71dd21e7c072d15f44d58e0292d1
Kaveh Vahedipour <kaveh@arangodb.com>
2022-10-14 10:48:24 +0200

    oh my oh my first commit

---
+++ smartify_person.js
@@ -0,0 +1,30 @@
+var fs = require('fs'),
+JSONStream = require('JSONStream'),
+es = require('event-stream')
+data = [],
+fname = "person_8b0a44048f58988b486bdd0d245b22a8.data.json",
+getStream = function (fname) {
+  var jsonData = fname,
+      stream = fs.createReadStream(jsonData, { encoding: 'utf8' }),
+      parser = JSONStream.parse('*');
+  return stream.pipe(parser);
+},
+getStream(fname).pipe(es.mapSync(function (d) {
+  if (d.hasOwnProperty("_key")) {
+    d.shard_by = d.orgId.substring(0,2);
+    d._key = d.shard_by + ":" + d._key;
+    data.push(d);
+  }
+})).then( function () { fs.writeFileSync(fname + ".new", JSON.stringify(data)); } )
+
+var outputStream = fs.createWriteStream(fname + ".new"),
+var transformStream = JSONStream.stringify();
+transformStream.pipe( outputStream ),
+data.forEach (transformStream.write);
+transformStream.end();
+outputStream.on(
+  "finish",
+  function handleFinish() {
+    console.log("Done");
+  }
+);
