ed9d5e737cdce981dc7f044943dccf2bfdae1e01
Kaveh Vahedipour <kaveh@arangodb.com>
2022-10-14 10:56:40 +0200

    fixed

--- smartify_person.js
+++ smartify_person.js
@@ -9,13 +9,13 @@ getStream = function (fname) {
       parser = JSONStream.parse('*');
   return stream.pipe(parser);
 },
-getStream(fname).pipe(es.mapSync(function (d) {
+getStream("../dump/" + fname).pipe(es.mapSync(function (d) {
   if (d.hasOwnProperty("_key")) {
     d.shard_by = d.orgId.substring(0,2);
     d._key = d.shard_by + ":" + d._key;
     data.push(d);
   }
-})).then( function () { fs.writeFileSync(fname + ".new", JSON.stringify(data)); } )
+})).then( function () {  console.log("Done reading.")} )
 
 var outputStream = fs.createWriteStream(fname + ".new"),
 var transformStream = JSONStream.stringify();
@@ -25,6 +25,6 @@ transformStream.end();
 outputStream.on(
   "finish",
   function handleFinish() {
-    console.log("Done");
+    console.log("Done writing");
   }
 );
